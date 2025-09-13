5b909d7383051ed7c591f4a424b1b8259f505f0f
Muhaiminul-Islam-Kanon75 <mikanon37@gmail.com>
2025-09-13 18:41:24 +0600

    2025-09-13 06:41:24 PM docs(k8s-manifest): update manifest files

---
+++ shellScripts/telegram_alertBot/telegram-02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://api.telegram.org/bot8412711666:AAHO8F1mTQt_yH185APvHYrnN5t0ff5IiQE/getUpdates
+
+BOT_TOKEN="8412711666:AAHO8F1mTQt_yH185APvHYrnN5t0ff5IiQE"
+CHAT_ID="2032649319"
+MESSAGE="
+test message 
+how are you 
+how is everything going"
+
+curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
+  -d chat_id=$CHAT_ID \
+  -d text="$MESSAGE"
+  
+
